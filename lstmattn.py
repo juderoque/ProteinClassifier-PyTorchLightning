967532b6d6d39874d10462131c1003bba22d2773
Jude Fernandes <juderoque@gmail.com>
2020-07-21 00:10:01 -0700

    Lightning port complete

--- lstmattn.py
+++ lstmattn.py
@@ -1,23 +1,45 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import pytorch_lightning as pl
-from utils.layers import MultiHeadAttention, PositionwiseFeedForward, EncoderLayer, PositionalEncoding
+from utils.layers import LinearClassifier, MaskedLanguageModel, EncoderLayer, PositionalEncoding
+import numpy as np
 
 
 class LSTMAttn(pl.LightningModule):
-    def __init__(self, embedding_dim, hidden_dim, seq_len, vocab_size, tagset_size,
-                 padding_idx=0, d_k=64, d_v=64, n_lstm=3, n_head=12, n_attn=2, dropout=0.1, need_pos_enc=True):
-
+    def __init__(self, embedding_dim,
+                 hidden_dim,
+                 seq_len,
+                 vocab,
+                 tagset_size,
+                 lr=1e-4,
+                 padding_idx=0,
+                 d_k=64,
+                 d_v=64,
+                 n_lstm=3,
+                 n_head=12,
+                 n_attn=2,
+                 dropout=0.1,
+                 need_pos_enc=True,
+                 mask_ratio=0.1,
+                 cls_weight=0.5,
+                 mlm_weight=0.5):
         super(LSTMAttn, self).__init__()
+
+        self.lr = lr
+
         self.hidden_dim = hidden_dim
         self.d_k = d_k
         self.n_head = n_head
         self.n_attn = n_attn
         self.n_lstm = n_lstm
         self.need_pos_enc = need_pos_enc
+        self.mask_ratio = mask_ratio
+        self.vocab = vocab
+        self.vocab_size = len(vocab)
+        self.cls_weight = cls_weight
+        self.mlm_weight = mlm_weight
 
-        self.word_embeddings = nn.Embedding(vocab_size, embedding_dim, padding_idx=padding_idx)
+        self.word_embeddings = nn.Embedding(self.vocab_size, embedding_dim, padding_idx=padding_idx)
 
         if need_pos_enc:
             self.position_enc = PositionalEncoding(embedding_dim, n_position=seq_len)
@@ -30,30 +52,46 @@ class LSTMAttn(pl.LightningModule):
             # we treat LSTM as transparent.
             # Divide 2 because the bi-LSTM actually output hidden_dim * 2
             self.hidden_dim = embedding_dim // 2
-
         if n_attn:
             self.hid2hid = nn.Linear(seq_len, 1)
         else:
             self.hid2hid = nn.Linear(self.hidden_dim * 2, self.hidden_dim * 2)
 
-        self.slf_attn_encoder = EncoderLayer(n_head, self.hidden_dim, seq_len, d_k, d_v, dropout=dropout)
-        self.hidden2tag = nn.Linear(self.hidden_dim * 2, tagset_size)
+        # self.slf_attn_encoder = EncoderLayer(n_head, self.hidden_dim, seq_len, d_k, d_v, dropout=dropout)
+        self.slf_attn_encoder_blocks = nn.ModuleList([
+            EncoderLayer(n_head, self.hidden_dim, seq_len, d_k, d_v, dropout=dropout)
+            for _ in range(n_attn)])
+        self.linear_classifier = LinearClassifier(self.hidden_dim, tagset_size)
+
+        # because of the bi-LSTM the total hidden_dim should be *2
+        # if not using bi-LSTM, the hidden_dim is first devided by 2
+        self.masked_language_model = MaskedLanguageModel(
+            self.hidden_dim * 2, self.vocab_size)
         self.dropout = nn.Dropout(p=dropout)
         self.padding_idx = padding_idx
         self.softmax = nn.Softmax()
         self.relu = nn.ReLU(inplace=True)
 
-    def forward(self, X, slf_attn_mask=None):
-        b_size, seq_len = X.size()
+    def forward(self, x, token_mask=None):
+        b_size, seq_len = x.size()
+
+        # creating mask and map the mask to the input
+        if token_mask is not None:
+            mask_label = self.vocab["<MASK>"]
+            x_input = x.masked_fill(token_mask == 0, mask_label)
+        else:
+            x_input = x
 
         # embedding + LSTM part
-        enc_output = self.word_embeddings(X)
+        enc_output = self.word_embeddings(x_input)
         if self.need_pos_enc:
             enc_output = self.position_enc(enc_output)
-        enc_output = self.dropout(enc_output)
-
+        # enc_output = self.dropout(enc_output)
         if self.n_lstm:
             # here "if" is for flexibility of taking LSTM or not.
+            # enc_output, _ = self.lstm(enc_output.view(b_size, seq_len, -1))
+            # u will want to send length only to the max size of current batch
+            # to the LSTM model
             enc_output, _ = self.lstm(enc_output.view(b_size, seq_len, -1))
 
         enc_output = enc_output.transpose(1, 2)
@@ -64,41 +102,105 @@ class LSTMAttn(pl.LightningModule):
         ###### self attention version ######
         if self.n_attn:
             attn_out = enc_output
-            for _ in range(self.n_attn):
-                attn_out, slf_attn_res = self.slf_attn_encoder(attn_out)
+            for slf_attn_encoder in self.slf_attn_encoder_blocks:
+                attn_out, slf_attn_res = slf_attn_encoder(attn_out)
         ####################################
         else:
             # default, take the last layer as output of LSTM
-            attn_out = embeds.mean(2)
+            attn_out = enc_output
         # linear transformation and classification layer
-        hidden_res = attn_out.mean(2)
-        hidden_res = hidden_res.view(b_size, self.hidden_dim * 2)
-        tag_space = self.hidden2tag(hidden_res)
-        tag_scores = F.log_softmax(tag_space, dim=1)
-        return tag_scores
+
+        tag_scores, mlm_scores = None, None
+        if self.cls_weight:
+            tag_scores = self.linear_classifier(attn_out)
+
+        # masked language model part
+        if self.mlm_weight:
+            mlm_scores = self.masked_language_model(attn_out)
+
+        return tag_scores, mlm_scores
 
     def training_step(self, batch, batch_idx):
-        x, y = batch
-        y_hat = self(x)
-        return {'loss': F.cross_entropy(y_hat, y)}
+
+        class_loss_function = nn.NLLLoss()
+        lm_loss_function = nn.NLLLoss(ignore_index=0)
+
+        x, target, token_mask = batch
+        tag_scores, mlm_scores = self(x, token_mask)
+        # analyze the loss
+        if self.cls_weight:
+            cls_loss = class_loss_function(tag_scores, target)
+        else:
+            cls_loss = 0.0
+
+        # we only compute cross entropy for the masked: mask==1 part
+        # we mask all the other tokens to be 0 and since the loss function
+        # will ignore all the place of 0, it's actually only considering
+        # the content within the mask
+        if self.mlm_weight:
+            x_mask = x.masked_fill(token_mask == 1, 0)
+            mlm_loss = lm_loss_function(mlm_scores, x_mask)
+        else:
+            mlm_loss = 0.0
+
+        loss = (self.cls_weight * cls_loss + self.mlm_weight * mlm_loss) / (self.cls_weight + self.mlm_weight)
+        return {'loss': loss}
+
+    def validation_step(self, batch, batch_idx):
+
+        x, target, token_mask = batch
+        target = target.numpy()
+        score_pred, mlm_pred = model(x)
+
+        if score_pred is not None:
+            cls_pred_fold = np.array(torch.max(score_pred, 1)[1].tolist())
+            cls_acc = sum(target == cls_pred_fold) / len(target)
+
+        if mlm_pred is not None:
+            mlm_pred_fold = torch.max(mlm_pred, 1)[1]
+            # this is the compare result
+            mlm_comp_res = mlm_pred_fold == x
+            # this is where we care, we will set these position to be 1
+            care_part = token_mask == 0
+            mlm_masked_res = mlm_comp_res & care_part
+
+            mlm_acc = np.sum(mlm_masked_res.tolist()) / np.sum(care_part.tolist())
+
+        return {'mlm_acc': torch.tensor(mlm_acc or 0, dtype=torch.float64), 'cls_acc': torch.tensor(cls_acc or 0, dtype=torch.float64)}
+
+    def validation_epoch_end(self, outputs):
+        mlm_acc = torch.stack([x['mlm_acc'] for x in outputs]).mean()
+        cls_acc = torch.stack([x['cls_acc'] for x in outputs]).mean()
+        log = {'mlm_acc': mlm_acc, 'cls_acc': cls_acc}
+        return {'log': log}
 
     def configure_optimizers(self):
-        return torch.optim.Adam(self.parameters(), lr=1e-4)
+        return torch.optim.Adam(self.parameters(), lr=self.lr)
 
 
 if __name__ == '__main__':
     from utils.dataset import PfamDataset
+    import argparse
+    from pytorch_lightning.loggers import WandbLogger
+
+    wandb_logger = WandbLogger(name='Trial', project='lstm-lightning')
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--batch_size', default=128, type=int)
+    parser.add_argument('--learning_rate', default=1e-4, type=float)
+    args = parser.parse_args()
+
     data_dir = 'data/random_split'
     sample_every = 100
     padding = 3000
 
     dataset = PfamDataset(data_dir, sample_every, padding)
-    model = LSTMAttn(80, 200, padding, dataset.vocab_size, dataset.target_size)
+    model = LSTMAttn(80, 200, padding, dataset.vocab, dataset.target_size, args.learning_rate)
 
-    train_loader = dataset.get_train_loader(batch_size=2)
-    val_loader = dataset.get_val_loader(batch_size=2)
+    train_loader = dataset.get_train_loader(batch_size=args.batch_size)
+    val_loader = dataset.get_val_loader(batch_size=args.batch_size)
 
-    trainer = pl.Trainer(fast_dev_run=True)
-    trainer.fit(model, train_dataloader=train_loader)
+    trainer = pl.Trainer(max_epochs=1, logger=wandb_logger)
+    trainer.fit(model, train_dataloader=train_loader, val_dataloaders=val_loader)
 
 
--- utils/dataset.py
+++ utils/dataset.py
@@ -30,12 +30,22 @@ def prepare_sequence(seq, vocab, padding):
     return idxs
 
 
+def create_mask(sequence, padding, mask_ratio):
+    true_len = len(sequence)
+    mask = [1]*padding
+    start, end = int(true_len * (1 - mask_ratio)), true_len
+    for i in range(start, end):
+        mask[i] = 0
+    return mask
+
+
 class PfamPartition(Dataset):
-    def __init__(self, data, fams, vocab, padding, fam_vocab):
+    def __init__(self, data, fams, vocab, padding, fam_vocab, mask_ratio):
         partition = data[data["family_id"].isin(fams)]
         x_raw = partition['aligned_sequence'].values
         y_raw = partition['family_id'].values
         self.x_data = torch.tensor([prepare_sequence(x, vocab, padding) for x in x_raw])
+        self.mask = torch.tensor([create_mask(x, padding, mask_ratio) for x in x_raw])
         self.y_data = torch.tensor([fam_vocab[y] for y in y_raw])
         self.length = self.x_data.shape[0]
 
@@ -43,14 +53,14 @@ class PfamPartition(Dataset):
         return self.length
 
     def __getitem__(self, idx):
-        return self.x_data[idx], self.y_data[idx]
+        return self.x_data[idx], self.y_data[idx], self.mask[idx]
 
 
 class PfamDataset:
-    def __init__(self, data_dir='data/random_split', sample_every=1, padding=3000):
-        vocab = {"<PAD>": 0, ".": 1, "M": 2, "H": 3, "B": 4, "W": 5, "R": 6, "U": 7, "I": 8, "O": 9, "L": 10,
-                      "T": 11, "D": 12, "F": 13, "X": 14, "Q": 15, "K": 16, "N": 17, "A": 18, "E": 19, "Y": 20, "V": 21,
-                      "Z": 22, "S": 23, "P": 24, "C": 25, "G": 26}
+    def __init__(self, data_dir='data/random_split', sample_every=1, padding=3000, mask_ratio=0.1):
+        self.vocab = {"<PAD>": 0, ".": 1, "M": 2, "H": 3, "B": 4, "W": 5, "R": 6, "U": 7, "I": 8, "O": 9, "L": 10, "T": 11,
+                 "D": 12, "F": 13, "X": 14, "Q": 15, "K": 16, "N": 17, "A": 18, "E": 19, "Y": 20, "V": 21, "Z": 22,
+                 "S": 23, "P": 24, "C": 25, "G": 26, "<MASK>": 27}
 
         train = read_all_shards('train', data_dir)
         val = read_all_shards('dev', data_dir)
@@ -59,29 +69,28 @@ class PfamDataset:
         fams = np.array(train["family_id"].value_counts().index)[::sample_every]
         fam_vocab = {fam: idx for idx, fam in enumerate(fams)}
 
-        self.train_set = PfamPartition(train, fams, vocab, padding, fam_vocab)
-        self.val_set = PfamPartition(val, fams, vocab, padding, fam_vocab)
-        self.test_set = PfamPartition(test, fams, vocab, padding, fam_vocab)
+        self.train_set = PfamPartition(train, fams, self.vocab, padding, fam_vocab, mask_ratio)
+        self.val_set = PfamPartition(val, fams, self.vocab, padding, fam_vocab, mask_ratio)
+        self.test_set = PfamPartition(test, fams, self.vocab, padding, fam_vocab, mask_ratio)
 
-        self.vocab_size = len(vocab)
         self.target_size = len(fams)
 
     def get_train_loader(self, batch_size=128, shuffle=True, num_workers=cpu_count()):
         return DataLoader(self.train_set, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
 
-    def get_val_loader(self, batch_size=128, shuffle=True, num_workers=cpu_count()):
+    def get_val_loader(self, batch_size=128, shuffle=False, num_workers=cpu_count()):
         return DataLoader(self.val_set, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
 
-    def get_test_loader(self, batch_size=128, shuffle=True, num_workers=cpu_count()):
+    def get_test_loader(self, batch_size=128, shuffle=False, num_workers=cpu_count()):
         return DataLoader(self.test_set, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
 
 
 if __name__ == "__main__":
-    dataset = PfamDataset(sample_every=1)
+    dataset = PfamDataset(sample_every=10)
     print(dataset.target_size)
-    #train_loader = dataset.get_train_loader()
-    #for i, data in enumerate(train_loader, 0):
-    #    inputs, labels = data
-    #    print(inputs.shape, labels.shape)
-    #    if i == 5:
-    #        break
+    train_loader = dataset.get_train_loader()
+    for i, data in enumerate(train_loader, 0):
+        inputs, labels, mask = data
+        print(inputs.shape, labels.shape, mask.shape)
+        if i == 5:
+            break
